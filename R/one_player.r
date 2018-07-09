9d637e1a6eb68452ca5ea1131a91d4248ea64039
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2018-07-09 13:41:31 +0200

    updates

--- R/one_player.r
+++ R/one_player.r
@@ -7,21 +7,29 @@
 example.solve.single.player.tg = function() {
   setwd("D:/libraries/gtree/myproject")
 
-	gameId = "UG3"
+	gameId = "UG2"
 	tg.org = get.tg(gameId = gameId, never.load = !FALSE)
 	eq.li = get.eq(tg = tg.org, never.load=TRUE)
-  eq = eq.li[[1]]
+  expected.eq.outcomes(eq.li=eq.li, tg=tg.org)
 
-  tg = make.best.reply.tg(tg.org, eq, player=2, tremble.prob = 1/100)
+  eq = eq.li[[2]]
+  tg = make.best.reply.tg(tg.org, eq, player=2, tremble.prob = 1/1000)
+  tg0 = make.best.reply.tg(tg.org, eq, player=2, tremble.prob = 0)
   eq = compute.single.player.eq(tg, player=2, find.all.eq=FALSE)
-  eq.li = compute.single.player.eq(tg, player=2, find.all.eq=TRUE)
+  eq0 = compute.single.player.eq(tg, player=2, find.all.eq=FALSE, eq=eq,zero.prob.info.sets = "old.strat")
+
+  expected.eq.outcomes(eq.li=list(eq), tg=tg)
+  expected.eq.outcomes(eq.li=list(eq0), tg=tg)
+
+
+  eq.li = compute.single.player.eq(tg, player=2, find.all.eq=FALSE)
+
+  expected.eq.outcomes(eq.li=eq.li, tg=tg)
 
 }
 
-compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.just.info.set.probs=FALSE, find.all.eq=FALSE) {
+compute.single.player.eq = function(tg, player=1, info.set.probs = attr(eq,".info.set.probs"), return.just.info.set.probs=FALSE, find.all.eq=FALSE, zero.prob.info.sets = c("uniform","old.strat",  "error")[1], eq=NULL,util.col = c(paste0("util_",player))) {
   restore.point("compute.single.player.eq")
-
-  util.cols = c(paste0("util_",1:tg$numPlayers))
 
   # We write one equilibrium into info.set.probs
   if (is.null(info.set.probs)) {
@@ -41,6 +49,7 @@ compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.
 
   # Outcomes that will be reduced via backward induction
   odf = tg$oco.df
+  odf$.util = odf[[util.col]]
 
   # Go through levels backwards
   lev.num = length(tg$lev.li)+1
@@ -55,7 +64,7 @@ compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.
     # add them later again to odf
     omitted.odf = anti_join(odf, lev.df, by=join.cols)
     # Join utilities to lev.df
-    lev.df = inner_join(lev.df, odf[,c(join.cols, util.cols)],
+    lev.df = inner_join(lev.df, odf[,c(join.cols, ".util")],
       by=join.cols)
 
     if (lev$type=="nature") {
@@ -64,7 +73,7 @@ compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.
       # by grouping over .node.ind
       odf  = lev.df %>%
         group_by_at(c(setdiff(join.cols, lev$var),".node.ind")) %>%
-        summarize_at(util.cols, funs(weighted.mean(., .move.prob))) %>%
+        summarize(.util= weighted.mean(.util, .move.prob)) %>%
         ungroup() %>%
         select(-.node.ind)
 
@@ -73,29 +82,55 @@ compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.
       # total probability over all nodes that it will
       # be reached
       lev.df = lev.df %>% group_by(.info.set.ind) %>%
-        mutate(.info.set.prob = sum(.prob * (.move.ind==1))) %>%
+        mutate(
+          .info.set.prob = sum(.prob * (.move.ind==1)),
+          .num.nodes = n_distinct(.node.ind)+1,
+          .undefined.node.prob =  .num.nodes >1 & .info.set.prob == 0
+        ) %>%
         ungroup() %>%
         # Compute the probability of each node given that
         # its information set is reached
-        # TO DO: If an information set is reached with zero prob,
-        # we have to assign some probabilities
-        mutate(.node.prob.in.info.set = .prob / .info.set.prob)
+        # If an information set with more than one node
+        # is reached with zero prob,
+        # we set .node.prob.in.info.set to NA and deal later with it
+        mutate(.node.prob.in.info.set = ifelse(.num.nodes > 1, .prob / .info.set.prob, 1))
+
+      if (zero.prob.info.sets=="error") {
+        if (any(lev.df$.undefined.node.prob)) {
+          stop("Some multi-node information sets are reached with zero probability. Cannot continue via Bayesian updating. Either create a one player game with zero.prob.info.sets=='uniform'")
+        }
+      } else if (zero.prob.info.sets=="uniform") {
+        # Assume node probabilities with each
+        # information set reached with zero prob
+        # are uniform
+        lev.df = lev.df %>%
+          mutate( .node.prob.in.info.set = ifelse(!is.finite(.node.prob.in.info.set),1 / .num.nodes, .node.prob.in.info.set))
+      } else if (zero.prob.info.sets=="old.strat") {
+        # Just remove the information sets that are reached with
+        # zero probability
+        #
+        # This keeps the old move probabilities
+        # from .info.set.probs
+        #
+        # Since the information sets are reached with zero prob
+        # it (hopefully) does not matter for expected utilities in
+        # earlier levels that we have just removed the rows
+        # (NEED TO TEST IF INDEED NO ERROR ARISES)
+        lev.df = filter(lev.df, !.undefined.node.prob)
+      }
 
       # Now compute for each .move.ind in each information set
       # the expected utilities
       temp.df  = lev.df %>%
         group_by(.info.set.ind, .move.ind) %>%
-        summarize_at(util.cols, funs(weighted.mean(., .node.prob.in.info.set))) %>%
+        summarize(.util = weighted.mean(.util, .node.prob.in.info.set)) %>%
         ungroup()
-
-      # Set relevant decision utility to the utility of the player
-      temp.df[[".util"]] = temp.df[[paste0("util_", player)]]
 
       # Find a single equilibrium
       single.eq.df = temp.df %>% group_by(.info.set.ind) %>%
         mutate(.best.move = .move.ind[which.max(.util)]) %>%
         filter(.move.ind == .best.move) %>%
-        select(-.util, -.best.move) %>%
+        select(-.best.move) %>%
         ungroup()
 
       # Join on equilibrium back to lev.df
@@ -110,6 +145,7 @@ compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.
         info.set.probs[unique(odf$.info.set.move.ind)] = 1
       } else {
         restore.point("make.all.eq")
+
 
         all.eq.df = temp.df %>% group_by(.info.set.ind) %>%
           mutate(.is.best.move = .util == max(.util)) %>%
@@ -164,6 +200,3 @@ compute.single.player.eq = function(tg, player=1, info.set.probs = NULL, return.
 
 }
 
-single.player.reduce.nature.level = function(lev, odf, tg) {
-
-}
